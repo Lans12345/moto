b898f6ffb8e1791ea42bf5db146ff8e47ded5ecb
Lans12345 <olanalans12345@gmail.com>
2023-01-02 07:37:49 +0800

    added leaderbord

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:moto/widgets/appbar_widget.dart';
+import 'package:moto/widgets/text_widget.dart';
 
 class LeaderBoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppbarWidget('Leaderboard for Categ Type'),
+      body: SingleChildScrollView(
+        child: DataTable(columns: [
+          DataColumn(
+            label: TextRegular(text: 'Name', fontSize: 18, color: Colors.black),
+          ),
+          DataColumn(
+            label:
+                TextRegular(text: 'Number', fontSize: 18, color: Colors.black),
+          ),
+          DataColumn(
+            label:
+                TextRegular(text: 'Score', fontSize: 18, color: Colors.black),
+          ),
+        ], rows: [
+          for (int i = 0; i < 50; i++)
+            DataRow(cells: [
+              DataCell(
+                TextRegular(text: 'John Doe', fontSize: 14, color: Colors.grey),
+              ),
+              DataCell(
+                TextRegular(text: '23', fontSize: 14, color: Colors.grey),
+              ),
+              DataCell(
+                TextRegular(
+                    text: '100' 'pts', fontSize: 14, color: Colors.grey),
+              ),
+            ]),
+        ]),
+      ),
     );
   }
 }
