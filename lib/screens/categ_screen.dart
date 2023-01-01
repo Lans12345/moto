647bc5583f974d7be16969f597d979ea859cf360
Lans12345 <olanalans12345@gmail.com>
2023-01-02 07:30:05 +0800

    added datatable

--- lib/screens/categ_screen.dart
+++ lib/screens/categ_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moto/widgets/appbar_widget.dart';
+import 'package:moto/widgets/button_widget.dart';
 import 'package:moto/widgets/text_widget.dart';
 
 class CategScreen extends StatelessWidget {
@@ -48,109 +49,126 @@ class CategScreen extends StatelessWidget {
               ]),
             ),
           ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 300,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MaterialButton(
-                        height: 40,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        minWidth: 130,
-                        color: Colors.black,
-                        onPressed: () {
-                          showDialog(
-                              context: context,
-                              builder: (context) {
-                                return Dialog(
-                                  child: SizedBox(
-                                    height: 300,
-                                    child: Center(
-                                      child: Column(
-                                        children: [
-                                          const SizedBox(
-                                            height: 20,
-                                          ),
-                                          TextBold(
-                                              text: 'Adding Entry',
-                                              fontSize: 18,
-                                              color: Colors.black),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                20, 5, 20, 5),
-                                            child: TextFormField(
-                                              onChanged: (input) {
-                                                name = input;
-                                              },
-                                              decoration: const InputDecoration(
-                                                  labelText: 'Name: '),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MaterialButton(
+                          height: 40,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minWidth: 130,
+                          color: Colors.black,
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    child: SizedBox(
+                                      height: 300,
+                                      child: Center(
+                                        child: Column(
+                                          children: [
+                                            const SizedBox(
+                                              height: 20,
                                             ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                20, 5, 20, 5),
-                                            child: TextFormField(
-                                              onChanged: (input) {
-                                                number = input;
-                                              },
-                                              keyboardType:
-                                                  TextInputType.number,
-                                              decoration: const InputDecoration(
-                                                  labelText: 'Number: '),
+                                            TextBold(
+                                                text: 'Adding Entry',
+                                                fontSize: 18,
+                                                color: Colors.black),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      20, 5, 20, 5),
+                                              child: TextFormField(
+                                                onChanged: (input) {
+                                                  name = input;
+                                                },
+                                                decoration:
+                                                    const InputDecoration(
+                                                        labelText: 'Name: '),
+                                              ),
                                             ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                20, 5, 20, 5),
-                                            child: TextFormField(
-                                              onChanged: (input) {
-                                                score = input;
-                                              },
-                                              keyboardType:
-                                                  TextInputType.number,
-                                              decoration: const InputDecoration(
-                                                  labelText: 'Score: '),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      20, 5, 20, 5),
+                                              child: TextFormField(
+                                                onChanged: (input) {
+                                                  number = input;
+                                                },
+                                                keyboardType:
+                                                    TextInputType.number,
+                                                decoration:
+                                                    const InputDecoration(
+                                                        labelText: 'Number: '),
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      20, 5, 20, 5),
+                                              child: TextFormField(
+                                                onChanged: (input) {
+                                                  score = input;
+                                                },
+                                                keyboardType:
+                                                    TextInputType.number,
+                                                decoration:
+                                                    const InputDecoration(
+                                                        labelText: 'Score: '),
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 10,
+                                            ),
+                                            ButtonWidget(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                text: 'Continue'),
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                );
-                              });
-                        },
-                        child: TextBold(
-                            text: 'Add Entry',
-                            fontSize: 14,
-                            color: Colors.white),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      MaterialButton(
-                        height: 40,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                                  );
+                                });
+                          },
+                          child: TextBold(
+                              text: 'Add Entry',
+                              fontSize: 14,
+                              color: Colors.white),
                         ),
-                        minWidth: 130,
-                        color: Colors.black,
-                        onPressed: () {},
-                        child: TextBold(
-                            text: 'View Leaderbord',
-                            fontSize: 14,
-                            color: Colors.white),
-                      ),
-                    ],
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        MaterialButton(
+                          height: 40,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minWidth: 130,
+                          color: Colors.black,
+                          onPressed: () {},
+                          child: TextBold(
+                              text: 'View Leaderbord',
+                              fontSize: 14,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
