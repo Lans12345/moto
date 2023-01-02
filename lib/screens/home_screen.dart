afa9a3139673269c40a57b6533ccd33dd16ee6a6
Lans12345 <olanalans12345@gmail.com>
2023-01-02 19:35:49 +0800

    wip slider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:moto/screens/categ_screen.dart';
 import 'package:moto/screens/facts_screen.dart';
@@ -6,7 +7,49 @@ import 'package:moto/widgets/button_widget.dart';
 import 'package:moto/widgets/text_widget.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  var items = [
+    Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        height: 150,
+        width: 300,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/1.jpg'),
+            ),
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        height: 150,
+        width: 300,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/2.jpg'),
+            ),
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        height: 150,
+        width: 300,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/3.jpg'),
+            ),
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -108,31 +151,50 @@ class HomeScreen extends StatelessWidget {
                       text: 'Motocross Events',
                       fontSize: 18,
                       color: Colors.black),
-                  SizedBox(
-                    height: 180,
-                    width: 350,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: ((context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Container(
-                            height: 150,
-                            width: 300,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Colors.black),
-                            child: Center(
-                              child: TextBold(
-                                  text: 'Rules and\nRegulations',
-                                  fontSize: 18,
-                                  color: Colors.white),
-                            ),
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
+                  // SizedBox(
+                  //   height: 180,
+                  //   width: 350,
+                  //   child: ListView.builder(
+                  //     scrollDirection: Axis.horizontal,
+                  //     itemBuilder: ((context, index) {
+                  //       return Padding(
+                  //         padding: const EdgeInsets.all(5.0),
+                  //         child: Container(
+                  //           height: 150,
+                  //           width: 300,
+                  //           decoration: BoxDecoration(
+                  //               borderRadius: BorderRadius.circular(5),
+                  //               color: Colors.black),
+                  //           child: Center(
+                  //             child: TextBold(
+                  //                 text: 'Rules and\nRegulations',
+                  //                 fontSize: 18,
+                  //                 color: Colors.white),
+                  //           ),
+                  //         ),
+                  //       );
+                  //     }),
+                  //   ),
+                  // ),
+
+                  CarouselSlider(
+                      items: items,
+                      options: CarouselOptions(
+                        height: 400,
+                        aspectRatio: 16 / 9,
+                        viewportFraction: 0.8,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: true,
+                        autoPlayInterval: const Duration(seconds: 3),
+                        autoPlayAnimationDuration:
+                            const Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        enlargeFactor: 0.3,
+                        scrollDirection: Axis.horizontal,
+                      )),
                   const SizedBox(
                     height: 100,
                   ),
